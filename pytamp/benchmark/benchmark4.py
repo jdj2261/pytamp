bd960a893f8af07e59553fb51724aa04632c9613
jdj2261 <wlseowhd12@naver.com>
2022-07-19 13:29:37 +0900

    Added benchmark4

--- pytamp/benchmark/benchmark4.py
+++ pytamp/benchmark/benchmark4.py
@@ -22,8 +22,8 @@ class Benchmark4(Benchmark):
 
     def _load_objects(self):
         self.table_mesh = get_object_mesh('ben_table.stl')
-        self.cylinder_mesh = get_object_mesh('hanoi_cylinder.stl', scale=[0.3, 0.3, 1.0])
-        self.disk_mesh = get_object_mesh('hanoi_disk.stl')
+        self.cylinder_mesh = get_object_mesh('hanoi_cylinder.stl', scale=[0.9, 0.9, 1.0])
+        self.disk_mesh = get_object_mesh('hanoi_disk.stl', scale=[1.2, 1.2, 1.0])
 
         self.cylinder_mesh_bound = get_mesh_bounds(mesh=self.cylinder_mesh)
         self.disk_mesh_bound = get_mesh_bounds(mesh=self.disk_mesh)
@@ -41,16 +41,17 @@ class Benchmark4(Benchmark):
     def _load_scene(self):
         theta = np.linspace(-np.pi, np.pi, self.disk_num)
         for i in range(self.disk_num):
-            disk_pos = np.array([0.6, 0.25, self.table_height + self.disk_mesh_bound[1][2] + self.disk_heigh *i ])
+            disk_pos = np.array([0.59, 0.25, self.table_height + self.disk_mesh_bound[1][2] + self.disk_heigh *i ])
             disk_ori = Transform._to_quaternion([0, 0, theta[i]])
+            disk_ori = Transform._to_quaternion([0, 0, np.pi])
             self.disk_pose[i] = Transform(pos=self.disk_mesh.center_mass + disk_pos, rot=disk_ori)
             disk_name = "hanoi_disk_" + str(i)
-            hanoi_mesh = get_object_mesh(f'hanoi_disk.stl')
+            hanoi_mesh = get_object_mesh(f'hanoi_disk.stl', scale=[1.5-0.1*i, 1.5-0.1*i, 1])
             self.scene_mngr.add_object(name=disk_name, gtype="mesh", gparam=hanoi_mesh, h_mat=self.disk_pose[i].h_mat, color=[0., 1., 0.])
 
-        self.scene_mngr.add_object(name="cylinder_1", gtype="mesh", gparam=self.cylinder_mesh, h_mat=self.cylinder1_pose.h_mat, color=[1, 0., 0.])
-        self.scene_mngr.add_object(name="cylinder_2", gtype="mesh", gparam=self.cylinder_mesh, h_mat=self.cylinder2_pose.h_mat, color=[1, 0., 0.])
-        self.scene_mngr.add_object(name="cylinder_3", gtype="mesh", gparam=self.cylinder_mesh, h_mat=self.cylinder3_pose.h_mat, color=[1, 0., 0.])
+        self.scene_mngr.add_object(name="cylinder_1", gtype="mesh", gparam=self.cylinder_mesh, h_mat=self.cylinder1_pose.h_mat, color=[0., 0., 1.])
+        self.scene_mngr.add_object(name="cylinder_2", gtype="mesh", gparam=self.cylinder_mesh, h_mat=self.cylinder2_pose.h_mat, color=[0., 0., 1.])
+        self.scene_mngr.add_object(name="cylinder_3", gtype="mesh", gparam=self.cylinder_mesh, h_mat=self.cylinder3_pose.h_mat, color=[0., 0., 1.])
         self.scene_mngr.add_object(name="table", gtype="mesh", gparam=self.table_mesh, h_mat=self.table_pose.h_mat, color=[0.39, 0.263, 0.129])
         self.scene_mngr.add_robot(self.robot)
 
@@ -58,7 +59,6 @@ class Benchmark4(Benchmark):
         self.scene_mngr.set_logical_state("cylinder_2", ("on", "table"), ("static", True)) 
         self.scene_mngr.set_logical_state("cylinder_3", ("on", "table"), ("static", True)) 
         
-
         self.scene_mngr.set_logical_state("hanoi_disk_0", ("on", "table"))
         self.scene_mngr.set_logical_state("hanoi_disk_1", ("on", "hanoi_disk_0"))
         self.scene_mngr.set_logical_state("hanoi_disk_2", ("on", "hanoi_disk_1"))
