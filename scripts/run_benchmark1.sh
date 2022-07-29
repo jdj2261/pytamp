02f787794bbb97617f7b82531efbcc0edf9d7e84
jdj2261 <wlseowhd12@naver.com>
2022-07-29 17:04:41 +0900

    Modified run_behcmark1.sh

--- scripts/run_benchmark1.sh
+++ scripts/run_benchmark1.sh
@@ -1,5 +1,5 @@
 for i in {1..4}
 do
-    python3 ../examples/doosan/mcts/benchmark1_test.py --budgets 10 --max_depth 20 --algo bai_perturb --box_number 6 --seed $i &
+    python3 ../examples/doosan/mcts/benchmark1_test.py --budgets 2000 --max_depth 30 --algo bai_perturb --box_number 6 --seed $i &
 done
-python3 ../examples/doosan/mcts/benchmark1_test.py --budgets 10 --max_depth 20 --algo bai_perturb --box_number 6 --seed 5
+python3 ../examples/doosan/mcts/benchmark1_test.py --budgets 2000 --max_depth 30 --algo bai_perturb --box_number 6 --seed 5
